7a49464e57e6026e18acf2d45c1514157fc8541f
Michal Pawel Wadowski <80638034+Michau94@users.noreply.github.com>
2021-09-21 16:35:44 +0200

    Added form to GET censored word

--- index.php
+++ index.php
@@ -4,9 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+    <title>PHP BadWord</title>
 </head>
 <body>
+    <form action="index.php" method="GET">
+        <input type="text" name="word" placeholder="Insert word here..." value="dolor">
+        <button type="submit">Censor</button>
+    </form>
 
    <?php
    $text = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius veniam, veritatis nobis similique suscipit hic, recusandae, adipisci deserunt tenetur dolor voluptas quisquam alias soluta eos dolores voluptatum obcaecati perspiciatis facilis.';
