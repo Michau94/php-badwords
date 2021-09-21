3e0e25fdee0214400ea979a3ccc65fbfbf940bc0
Michal Pawel Wadowski <80638034+Michau94@users.noreply.github.com>
2021-09-21 14:22:51 +0200

    fix to replace only exact word

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
    $text = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius veniam, veritatis nobis similique suscipit hic, recusandae, adipisci deserunt tenetur dolor voluptas quisquam alias soluta eos dolores voluptatum obcaecati perspiciatis facilis.';
    $length = strlen($text);
    $censored = $_GET['word'];
-   $replaced = str_replace($censored, '***', $text);
+   $replaced = str_replace(' ' . $censored . ' ', ' *** ', $text);
    $lengthReplaced= strlen($replaced);
    
     ?>
@@ -20,15 +20,15 @@
     <h1>Paragrafo Originale</h1>
     <p>
         <?php echo $text; 
-        echo "Questo paragrafo è lungo: $length";
+        echo " Questo paragrafo è lungo: $length";
         ?>
     </p>
 
-    <h1>Paragrafo Modificato</h1>
+    <h1>Paragrafo Censurato</h1>
      <p>
         <?php        
         echo $replaced;
-        echo "Questo paragrafo è lungo: $lengthReplaced "; 
+        echo " Questo paragrafo è lungo: $lengthReplaced"; 
         ?>
      </p>
 </body>
