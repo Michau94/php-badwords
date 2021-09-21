afb68eec3b389735df5b55c88b6e8ad5f1f9bead
Michal Pawel Wadowski <80638034+Michau94@users.noreply.github.com>
2021-09-21 14:14:04 +0200

    added replaced paragraph with count

--- index.php
+++ index.php
@@ -11,9 +11,25 @@
    <?php
    $text = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius veniam, veritatis nobis similique suscipit hic, recusandae, adipisci deserunt tenetur dolor voluptas quisquam alias soluta eos dolores voluptatum obcaecati perspiciatis facilis.';
    $length = strlen($text);
+   $censored = $_GET['word'];
+   $replaced = str_replace($censored, '***', $text);
+   $lengthReplaced= strlen($replaced);
+   
     ?>
 
-    <p><?php echo $text; 
-     echo "Questo paragrafo è lungo: $length" ?></p>
+    <h1>Paragrafo Originale</h1>
+    <p>
+        <?php echo $text; 
+        echo "Questo paragrafo è lungo: $length";
+        ?>
+    </p>
+
+    <h1>Paragrafo Modificato</h1>
+     <p>
+        <?php        
+        echo $replaced;
+        echo "Questo paragrafo è lungo: $lengthReplaced "; 
+        ?>
+     </p>
 </body>
 </html>
